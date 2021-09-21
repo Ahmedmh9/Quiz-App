c0f02b4f57b1bce21cdace34fc554ba95da64e39
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-09-21 11:17:52 -0500

    selecting jquary elements

--- js/question.js
+++ js/question.js
@@ -1,89 +1,89 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
-      numb: 1,
-      question:
-        "Which built-in method returns the index within the calling String object of the first occurrence of the specified value??",
-      answer: "indexOf()",
-      options: ["getIndex()", "location()", "indexOf()", "None of the above."],
-    },
-    {
-      numb: 2,
-      question: "What does HTML stand for?",
-      answer: "Hyper Text Markup Language",
-      options: [
-        "Hyper Text Preprocessor",
-        "Hyper Text Markup Language",
-        "Hyper Text Multiple Language",
-        "Hyper Tool Multi Language",
-      ],
-    },
-    {
-      numb: 3,
-      question: "What does CSS stand for?",
-      answer: "Cascading Style Sheet",
-      options: [
-        "Common Style Sheet",
-        "Colorful Style Sheet",
-        "Computer Style Sheet",
-        "Cascading Style Sheet",
-      ],
-    },
-    {
-      numb: 4,
-      question: "Can you access Cookie using javascript??",
-      answer: "true",
-      options: ["tru", "false"],
-    },
-    {
-      numb: 5,
-      question: "What does SQL stand for?",
-      answer: "Structured Query Language",
-      options: [
-        "Stylish Question Language",
-        "Stylesheet Query Language",
-        "Statement Question Language",
-        "Structured Query Language",
-      ],
-    },
-    {
-      numb: 6,
-      question: "What does XML stand for?",
-      answer: "eXtensible Markup Language",
-      options: [
-        "eXtensible Markup Language",
-        "eXecutable Multiple Language",
-        "eXTra Multi-Program Language",
-        "eXamine Multiple Language",
-      ],
-    },
-    {
-      numb: 7,
-      question:
-        "Which built-in method reverses the order of the elements of an array?",
-      answer: "reverse()",
-      options: [
-        "changeOrder(order)",
-        "reverse()",
-        "sort(order)",
-        "None of the above",
-      ],
-    },
-    {
-      numb: 8,
-      question: "How do you call a function named 'myFunction?",
-      answer: "myFunction()",
-      options: [
-        "myFunction()",
-        "call function myFunction()",
-        "call myFunction()",
-        "None of the above",
-      ],
-    },
-    {
-      numb: 9,
-      question: "How to write an IF statement in javascript?",
-      answer: "if(i===5)",
-      options: ["if i =5", "if i = 5 then", "if(i===5)", "None of the above"],
-    },
-  ];
+  {
+    numb: 1,
+    question:
+      "Which built-in method returns the index within the calling String object of the first occurrence of the specified value??",
+    answer: "indexOf()",
+    options: ["getIndex()", "location()", "indexOf()", "None of the above."],
+  },
+  {
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 4,
+    question: "Can you access Cookie using javascript??",
+    answer: "true",
+    options: ["tru", "false"],
+  },
+  {
+    numb: 5,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+  {
+    numb: 6,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ],
+  },
+  {
+    numb: 7,
+    question:
+      "Which built-in method reverses the order of the elements of an array?",
+    answer: "reverse()",
+    options: [
+      "changeOrder(order)",
+      "reverse()",
+      "sort(order)",
+      "None of the above",
+    ],
+  },
+  {
+    numb: 8,
+    question: "How do you call a function named 'myFunction?",
+    answer: "myFunction()",
+    options: [
+      "myFunction()",
+      "call function myFunction()",
+      "call myFunction()",
+      "None of the above",
+    ],
+  },
+  {
+    numb: 9,
+    question: "How to write an IF statement in javascript?",
+    answer: "if(i===5)",
+    options: ["if i =5", "if i = 5 then", "if(i===5)", "None of the above"],
+  },
+];
